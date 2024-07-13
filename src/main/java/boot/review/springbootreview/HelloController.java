3ec136e2b415065949f3a54d4057fffbc4b6ec5d
yongjun96 <practice960426@gmail.com>
2024-07-13 18:13:37 +0900

    REFACTOR : frontController 사용
    1. servletContainer 를 frontController 로 수정
    2. 요청에서 받은 값을 메서드에 인자로 넘기는 바인딩 작업 추가
    3. 요청 path 별로 작업을 다르게 수행

---
+++ src/main/java/boot/review/springbootreview/HelloController.java
@@ -0,0 +1,9 @@
+package boot.review.springbootreview;
+
+public class HelloController {
+
+    public String hello(String name){
+        return "hello " + name;
+    }
+
+}
--- src/main/java/boot/review/springbootreview/SpringBootReviewApplication.java
+++ src/main/java/boot/review/springbootreview/SpringBootReviewApplication.java
@@ -8,6 +8,7 @@ import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactor
 import org.springframework.boot.web.server.WebServer;
 import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
 import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 
@@ -24,20 +25,39 @@ public class SpringBootReviewApplication {
 		// 다른 서버도 사용할 수 있도록 추상화 했기 때문에 webServer 를 사용
 		WebServer webServer = serverFactory.getWebServer(servletContext -> {
 
-			// servlet 생성
-            servletContext.addServlet("servlet", new HttpServlet() {
+			HelloController helloController = new HelloController();
+
+			// frontController 생성
+            servletContext.addServlet("frontController", new HttpServlet() {
 				@Override
 				protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-					// 호출 생성
-					String name = req.getParameter("name");
 
-					// 응답 생성
-					resp.setStatus(HttpStatus.OK.value());
-					resp.setHeader(HttpHeaders.CONTENT_TYPE, MediaType.TEXT_PLAIN_VALUE);
-					resp.getWriter().println("servlet Hello"+ name);
+					// 요청 생성 : 인증, 보안, 다국어 등... (공통 기능)
+
+					// GetMethod 로 들어 오는 경우만 처리
+					if(req.getRequestURI().equals("/servlet-request") && req.getMethod().equals(HttpMethod.GET.name())){
+						String name = req.getParameter("name");
+
+						// 요청 에서 받아온 Parameter 를 가지고 helloController 에 Method 에서 로직을 수행한 뒤 리턴 받을 수도 있음
+						String ret = helloController.hello(name);
+
+						// 응답 생성
+						resp.setStatus(HttpStatus.OK.value());
+						resp.setHeader(HttpHeaders.CONTENT_TYPE, MediaType.TEXT_PLAIN_VALUE);
+						resp.getWriter().println(ret);
+					}
+
+					else if (req.getRequestURI().equals("/user")) {
+						// 해당 요청의 응답 작성
+					}
+
+					else {
+						resp.setStatus(HttpStatus.NOT_FOUND.value());
+					}
+
 				}
-				// /servlet-request 이라는 path 로 요청이 들어 오면 생성한 servlet 으로 처리
-			}).addMapping("/servlet-request");
+				// 모든 요청에 대응할 수 있도록 /* 사용
+			}).addMapping("/*");
         });
 
 		// tomcat servlet container 실행

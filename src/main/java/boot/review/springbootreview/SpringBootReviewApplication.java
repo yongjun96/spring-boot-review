ff01d70903863574200b05d8ab880c4d668f05d8
yongjun96 <practice960426@gmail.com>
2024-07-13 17:50:51 +0900

    FEAT : Servlet, container 추가
    1. ServletContainer 를 생성
    2. 생성된 ServletContainer 에 servlet 를 생성
    3. servlet 을 mapping
    4. 호출과 응답 생성

--- src/main/java/boot/review/springbootreview/SpringBootReviewApplication.java
+++ src/main/java/boot/review/springbootreview/SpringBootReviewApplication.java
@@ -1,13 +1,46 @@
 package boot.review.springbootreview;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+import org.springframework.boot.web.server.WebServer;
+import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 
-@SpringBootApplication
+import java.io.IOException;
+
+
 public class SpringBootReviewApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SpringBootReviewApplication.class, args);
-	}
 
+		ServletWebServerFactory serverFactory = new TomcatServletWebServerFactory();
+
+		// servlet container 를 만드는 함수
+		// 다른 서버도 사용할 수 있도록 추상화 했기 때문에 webServer 를 사용
+		WebServer webServer = serverFactory.getWebServer(servletContext -> {
+
+			// servlet 생성
+            servletContext.addServlet("servlet", new HttpServlet() {
+				@Override
+				protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+					// 호출 생성
+					String name = req.getParameter("name");
+
+					// 응답 생성
+					resp.setStatus(HttpStatus.OK.value());
+					resp.setHeader(HttpHeaders.CONTENT_TYPE, MediaType.TEXT_PLAIN_VALUE);
+					resp.getWriter().println("servlet Hello"+ name);
+				}
+				// /servlet-request 이라는 path 로 요청이 들어 오면 생성한 servlet 으로 처리
+			}).addMapping("/servlet-request");
+        });
+
+		// tomcat servlet container 실행
+		webServer.start();
+	}
 }

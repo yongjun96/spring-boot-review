dc8dff7b3ba694158ebf10dba1d10246ae1208f3
yongjun96 <practice960426@gmail.com>
2024-07-14 21:22:40 +0900

    REFACTOR : DispatcherServlet 으로 수정
    1. servlet 을 DispatcherServlet 으로 생성
    2. controller 에 매핑을 추가
    3. controller 에 @ResponseBody 를 사용해서 응답 Body로 사용하도록 수정
    ps. 반환 타입을 String 으로 사용하게 되면 View page 로 반환하게 된다.
    RestController 애노테이션은 기본적으로 @ResponseBody 를 포함하도록 해준다.

--- src/main/java/boot/review/springbootreview/SpringBootReviewApplication.java
+++ src/main/java/boot/review/springbootreview/SpringBootReviewApplication.java
@@ -1,18 +1,10 @@
 package boot.review.springbootreview;
 
-import jakarta.servlet.ServletException;
-import jakarta.servlet.http.HttpServlet;
-import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
 import org.springframework.boot.web.server.WebServer;
 import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
-import org.springframework.context.support.GenericApplicationContext;
-import org.springframework.http.HttpMethod;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
-
-import java.io.IOException;
+import org.springframework.web.context.support.GenericWebApplicationContext;
+import org.springframework.web.servlet.DispatcherServlet;
 
 
 public class SpringBootReviewApplication {
@@ -20,7 +12,7 @@ public class SpringBootReviewApplication {
 	public static void main(String[] args) {
 
 		// Spring Container == ApplicationContext
-		GenericApplicationContext context = new GenericApplicationContext();
+		GenericWebApplicationContext context = new GenericWebApplicationContext();
 
 		// Bean 으로 등록
 		context.registerBean(HelloController.class);
@@ -36,33 +28,11 @@ public class SpringBootReviewApplication {
 		// 다른 서버도 사용할 수 있도록 추상화 했기 때문에 webServer 를 사용
 		WebServer webServer = serverFactory.getWebServer(servletContext -> {
 
-			// frontController 생성
-            servletContext.addServlet("frontController", new HttpServlet() {
-				@Override
-				protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-
-					// 요청 생성 : 인증, 보안, 다국어 등... (공통 기능)
-
-					// GetMethod 로 들어 오는 경우만 처리
-					if(req.getRequestURI().equals("/servlet-request") && req.getMethod().equals(HttpMethod.GET.name())){
-						String name = req.getParameter("name");
-
-						// 동록된 Bean 을 가져 와서 사용
-						HelloController helloController = context.getBean(HelloController.class);
-
-						String ret = helloController.hello(name);
-
-						// 응답 생성
-						resp.setContentType(MediaType.TEXT_PLAIN_VALUE);
-						resp.getWriter().println(ret);
-					}
-					else {
-						resp.setStatus(HttpStatus.NOT_FOUND.value());
-					}
-
-				}
-				// 모든 요청에 대응할 수 있도록 /* 사용
-			}).addMapping("/*");
+			// dispatcherServlet 생성
+            servletContext.addServlet("dispatcherServlet",
+						// 요청을 위임할 servlet container 지정
+						new DispatcherServlet(context))
+							.addMapping("/*");
         });
 
 		// tomcat servlet container 실행

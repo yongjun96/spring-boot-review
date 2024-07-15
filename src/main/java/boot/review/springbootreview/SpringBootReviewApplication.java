ee77cc6ebf9c74486001f0e45ef1c1dc7c26eed0
yongjun96 <practice960426@gmail.com>
2024-07-15 22:13:01 +0900

    REFACTOR : GenericWebApplicationContext 에서 DispatcherServlet 초기화
    1. GenericWebApplicationContext 확장
    2. onRefresh method 를 Override

--- src/main/java/boot/review/springbootreview/SpringBootReviewApplication.java
+++ src/main/java/boot/review/springbootreview/SpringBootReviewApplication.java
@@ -12,7 +12,29 @@ public class SpringBootReviewApplication {
 	public static void main(String[] args) {
 
 		// Spring Container == ApplicationContext
-		GenericWebApplicationContext context = new GenericWebApplicationContext();
+		GenericWebApplicationContext context = new GenericWebApplicationContext() {
+
+			@Override
+			protected void onRefresh() {
+				super.onRefresh();
+
+				ServletWebServerFactory serverFactory = new TomcatServletWebServerFactory();
+
+				// servlet container 를 만드는 함수
+				// 다른 서버도 사용할 수 있도록 추상화 했기 때문에 webServer 를 사용
+				WebServer webServer = serverFactory.getWebServer(servletContext -> {
+
+					// dispatcherServlet 생성
+					servletContext.addServlet("dispatcherServlet",
+									// 요청을 위임할 servlet container 지정
+									new DispatcherServlet(this))
+							.addMapping("/*");
+				});
+
+				// tomcat servlet container 실행
+				webServer.start();
+			}
+		};
 
 		// Bean 으로 등록
 		context.registerBean(HelloController.class);
@@ -20,22 +42,5 @@ public class SpringBootReviewApplication {
 
 		// 등록된 Bean 을 만들 어서 가지고 있음 (초기화 작업)
 		context.refresh();
-
-
-		ServletWebServerFactory serverFactory = new TomcatServletWebServerFactory();
-
-		// servlet container 를 만드는 함수
-		// 다른 서버도 사용할 수 있도록 추상화 했기 때문에 webServer 를 사용
-		WebServer webServer = serverFactory.getWebServer(servletContext -> {
-
-			// dispatcherServlet 생성
-            servletContext.addServlet("dispatcherServlet",
-						// 요청을 위임할 servlet container 지정
-						new DispatcherServlet(context))
-							.addMapping("/*");
-        });
-
-		// tomcat servlet container 실행
-		webServer.start();
 	}
 }

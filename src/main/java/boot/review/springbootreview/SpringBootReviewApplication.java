a5180bef32df1a6e25d4079f03c99feaf1e4b483
yongjun96 <practice960426@gmail.com>
2024-07-17 22:58:44 +0900

    REFACTOR : @Configuration 으로 수정
    1. @Configuration 를 사용하여 Bean 생성
    2. AnnotationConfigWebApplicationContext 를 사용 해서 servlet 에서 Bean 을 생성하는 대신 @Configuration 에 등록된 @Bean 사용

--- src/main/java/boot/review/springbootreview/SpringBootReviewApplication.java
+++ src/main/java/boot/review/springbootreview/SpringBootReviewApplication.java
@@ -3,16 +3,32 @@ package boot.review.springbootreview;
 import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
 import org.springframework.boot.web.server.WebServer;
 import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
-import org.springframework.web.context.support.GenericWebApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
 import org.springframework.web.servlet.DispatcherServlet;
 
-
+// 구성 정보를 가지고 있는 class 명시
+// Bean factory method 를 가지는 것 이상 으로 전체 application 을 구성 하는데 중요한 정보를 담음
+@Configuration
 public class SpringBootReviewApplication {
+
+	// java factory method
+	@Bean
+	public HelloController helloController(HelloService helloService){
+		return new HelloController(helloService);
+	}
+
+	// java factory method
+	@Bean
+	public HelloService helloService(){
+		return new SimpleHelloService();
+	}
 
 	public static void main(String[] args) {
 
-		// Spring Container == ApplicationContext
-		GenericWebApplicationContext context = new GenericWebApplicationContext() {
+		// AnnotationConfigWebApplicationContext -> @Configuration 가 붙은 class 를 이용 해서 설정 정보를 읽어 옴
+		AnnotationConfigWebApplicationContext context = new AnnotationConfigWebApplicationContext() {
 
 			@Override
 			protected void onRefresh() {
@@ -36,11 +52,8 @@ public class SpringBootReviewApplication {
 			}
 		};
 
-		// Bean 으로 등록
-		context.registerBean(HelloController.class);
-		context.registerBean(SimpleHelloService.class);
-
-		// 등록된 Bean 을 만들 어서 가지고 있음 (초기화 작업)
+		// java code 로 된 구성 정보 -> SpringBootReviewApplication 에 @Bean 을 사용
+		context.register(SpringBootReviewApplication.class);
 		context.refresh();
 	}
 }

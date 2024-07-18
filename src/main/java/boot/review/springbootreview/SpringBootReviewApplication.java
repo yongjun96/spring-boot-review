fec8760b729f76d21166a0e4081c90f3f5601223
yongjun96 <practice960426@gmail.com>
2024-07-18 22:13:35 +0900

    REFACTOR : SpringBoot 에서 사용하던 방식으로 MySpringApplication class 생성
    MySpringApplication 을 생성하고 Servlet 을 생성하던 코드를 옮겨 main method 에서 실행해 보기

--- src/main/java/boot/review/springbootreview/SpringBootReviewApplication.java
+++ src/main/java/boot/review/springbootreview/SpringBootReviewApplication.java
@@ -1,15 +1,11 @@
 package boot.review.springbootreview;
 
 import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
-import org.springframework.boot.web.server.WebServer;
 import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
 import org.springframework.web.servlet.DispatcherServlet;
-
-import javax.xml.crypto.dsig.spec.DigestMethodParameterSpec;
 
 // 구성 정보를 가지고 있는 class 명시
 // Bean factory method 를 가지는 것 이상 으로 전체 application 을 구성 하는데 중요한 정보를 담음
@@ -19,6 +15,11 @@ import javax.xml.crypto.dsig.spec.DigestMethodParameterSpec;
 @ComponentScan
 public class SpringBootReviewApplication {
 
+	public static void main(String[] args) {
+		MySpringApplication.run(SpringBootReviewApplication.class, args);
+	}
+
+
 	@Bean
 	public ServletWebServerFactory servletWebServerFactory(){
 		return new TomcatServletWebServerFactory();
@@ -27,36 +28,5 @@ public class SpringBootReviewApplication {
 	@Bean
 	public DispatcherServlet dispatcherServlet(){
 		return new DispatcherServlet();
-	}
-
-	public static void main(String[] args) {
-
-		// AnnotationConfigWebApplicationContext -> @Configuration 가 붙은 class 를 이용 해서 설정 정보를 읽어 옴
-		AnnotationConfigWebApplicationContext context = new AnnotationConfigWebApplicationContext() {
-
-			@Override
-			protected void onRefresh() {
-				super.onRefresh();
-
-				ServletWebServerFactory serverFactory = this.getBean(ServletWebServerFactory.class);
-				DispatcherServlet dispatcherServlet = this.getBean(DispatcherServlet.class);
-
-				// servlet container 를 만드는 함수
-				// 다른 서버도 사용할 수 있도록 추상화 했기 때문에 webServer 를 사용
-				WebServer webServer = serverFactory.getWebServer(servletContext -> {
-
-					// dispatcherServlet 생성
-					servletContext.addServlet("dispatcherServlet", dispatcherServlet)
-							.addMapping("/*");
-				});
-
-				// tomcat servlet container 실행
-				webServer.start();
-			}
-		};
-
-		// java code 로 된 구성 정보 -> SpringBootReviewApplication 에 @Bean 을 사용
-		context.register(SpringBootReviewApplication.class);
-		context.refresh();
 	}
 }

1e8214832a0c720b311323b308ccc795dd607641
yongjun96 <practice960426@gmail.com>
2024-07-21 00:15:40 +0900

    REFACTOR : HelloDecorator 우선 순위 적용
    1. 다른 곳에서 같은 Bean 을 주입 받으면 먼저 주입 받을 대상을 지정해 줘야 한다.
    2. HelloDecorator 에 @Primary 를 지정 해서 HelloService 를 먼저 주입 받도록 세팅

--- src/main/java/boot/review/springbootreview/HelloDecorator.java
+++ src/main/java/boot/review/springbootreview/HelloDecorator.java
@@ -1,8 +1,11 @@
 package boot.review.springbootreview;
 
+import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Service;
 
 @Service
+// Spring Container 에 같은 Bean 이 두개가 존재 하는 경우, @Primary 가 붙은 것을 우선 적으로 사용
+@Primary
 public class HelloDecorator implements HelloService {
 
     private final HelloService helloService;

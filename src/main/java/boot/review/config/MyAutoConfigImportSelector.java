ae3601569c8c7898f2315434d404e973dd09f827
yongjun96 <practice960426@gmail.com>
2024-11-10 17:30:14 +0900

    REFACTOR : 커스텀한 구성 요소를 저장하는 어노테이션 만들기

--- src/main/java/boot/review/config/MyAutoConfigImportSelector.java
+++ src/main/java/boot/review/config/MyAutoConfigImportSelector.java
@@ -1,16 +1,26 @@
 package boot.review.config;
 
+import org.springframework.boot.context.annotation.ImportCandidates;
 import org.springframework.context.annotation.DeferredImportSelector;
 import org.springframework.core.type.AnnotationMetadata;
 
+import java.util.stream.StreamSupport;
+
+
 public class MyAutoConfigImportSelector implements DeferredImportSelector {
+
+    private final ClassLoader classLoader;
+
+    public MyAutoConfigImportSelector(ClassLoader classLoader) {
+        this.classLoader = classLoader;
+    }
 
     // String 으로 리턴 시키면 String 값과 같은 class 이름을 가진 구성 정보를 import
     @Override
     public String[] selectImports(AnnotationMetadata importingClassMetadata) {
-        return new String[] {
-            "boot.review.config.autoconfig.DispatcherServletConfig",
-            "boot.review.config.autoconfig.TomcatWebServerConfig"
-        };
+        // 어노테이션의 클레스 object 넣음
+        // 자동 구성에 사용할 구성 요소들이 들어가 있게 됨
+        ImportCandidates candidates =  ImportCandidates.load(MyAutoConfiguration.class, classLoader);
+        return StreamSupport.stream(candidates.spliterator(), false).toArray(String[]::new);
     }
 }
